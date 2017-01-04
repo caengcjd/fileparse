1cd6572ec3addd726caf852814f62207b6c2b096
guodong <gd@tongjo.com>
2017-01-04 17:33:56 +0800

    Update test.js

--- test.js
+++ test.js
@@ -122,7 +122,7 @@ app.post('/parse', function(req, res, next) {
           });
           res_resolve.on('end', function() {
             console.log('响应结束********');
-            res_resolve.send({data: html, result: 1});
+            g_res.send({data: html, result: 1});
             return;
           });
         });
